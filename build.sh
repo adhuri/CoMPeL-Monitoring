eb5e8c84cd05e6d33a98ce90946045b383827d3d
adhuri <aniket.dhuri@gmail.com>
2017-02-05 01:15:05 -0500

    Pushed without tags

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+go build -o bin/compel-monitoring-server -i github.com/adhuri/CoMPeL-Monitoring/compel-monitoring-server
+go build -o bin/compel-monitoring-agent -i github.com/adhuri/CoMPeL-Monitoring/compel-monitoring-agent
