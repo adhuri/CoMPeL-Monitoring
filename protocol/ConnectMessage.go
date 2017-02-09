28429a0b883c8ac4cace247fa57224d4b58ca3ef
SaurabhSakpal <sfstoe@gmail.com>
2017-02-09 13:32:46 -0500

    Basic Message Structure

--- protocol/ConnectMessage.go
+++ protocol/ConnectMessage.go
@@ -1,3 +1,14 @@
 package protocol
 
-struct 
+import "net"
+
+type ConnectRequest struct {
+	messageId uint8
+	agentIP   net.IP
+	agentPort uint16
+}
+
+type ConnectReply struct {
+	messageId     uint8
+	isSuccessfull bool
+}
