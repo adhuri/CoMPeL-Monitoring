9001d3bffd3c0ee216a4e43ddbad4a1873507167
SaurabhSakpal <sfstoe@gmail.com>
2017-02-28 02:07:26 -0500

    adding functionality to save the total CPU and Server Stats

--- compel-monitoring-agent/Model.go
+++ compel-monitoring-agent/Model.go
@@ -11,14 +11,19 @@ var MEM_STATS = statType{"MEMORY"}
 var BLKIO_STATS = statType{"BLKIO"}
 
 type Client struct {
-	sync.Mutex
+	sync.RWMutex
 	contianerStats map[string]string
+	totalMemory    string
+	totalCPU       string
 }
 
+// This method accepts container ID and statType
+// It returns the value of the stats for the contianer
+// It aquires a reader lock before reading the map
 func (client *Client) GetStats(containerId string, stat statType) (string, error) {
-	key := containerId + stat.string
-	client.Lock()
-	defer client.Unlock()
+	key := containerId + stat.statName
+	client.RLock()
+	defer client.RUnlock()
 	value, present = contianerStats[key]
 	if present {
 		return value, nil
@@ -27,9 +32,39 @@ func (client *Client) GetStats(containerId string, stat statType) (string, error
 	}
 }
 
-func (*Client) SetStats(stat statType, containerId string, value string) {
+// This method accepts stat type, container id and value as input
+// It acquires a writer lock before updating the map
+func (client *Client) SetStats(stat statType, containerId string, value string) {
 	key := containerId + stat.statName
 	client.Lock()
 	defer client.Unlock()
 	contianerStats[key] = value
+}
+
+// returns total memory used by all the containers
+func (client *Client) GetTotalMemStats() string {
+	client.RLock()
+	defer client.RUnlock()
+	return client.totalMemory
+}
+
+// return total CPU cycles usued by all containers
+func (client *Client) GetTotalCPUStats() string {
+	client.RLock()
+	defer client.RUnlock()
+	return client.totalCPU
+}
+
+// sets total memory used by all the containers
+func (client *Client) SetTotalMemStats(value string) {
+	client.Lock()
+	defer client.Unlock()
+	client.totalMemory = value
+}
+
+// sets total CPU cycles used by all the containers
+func (client *Client) SetTotalCPUStats(value string) {
+	client.Lock()
+	defer client.Unlock()
+	client.totalCPU = value
 }
