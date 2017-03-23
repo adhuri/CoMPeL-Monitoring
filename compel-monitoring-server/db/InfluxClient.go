496451da02f5a828b03f146bd3dfd5dd58f92452
SaurabhSakpal <sfstoe@gmail.com>
2017-03-22 21:54:16 -0400

    Commenting out main method

--- compel-monitoring-server/db/InfluxClient.go
+++ compel-monitoring-server/db/InfluxClient.go
@@ -1,9 +1,7 @@
 package db
 
 import (
-	"fmt"
 	"log"
-	"strconv"
 	"time"
 
 	influx "github.com/influxdata/influxdb/client/v2"
@@ -81,34 +79,38 @@ func queryDB(clnt influx.Client, cmd string) (res []influx.Result, err error) {
 	return res, nil
 }
 
-func main() {
-	i, err := strconv.ParseInt("1490057610", 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	tm := time.Unix(i, 0)
-	fmt.Println(tm)
-
-	c, err := influx.NewHTTPClient(influx.HTTPConfig{
-		Addr:     "http://localhost:10090",
-		Username: username,
-		Password: password,
-	})
-
-	defer c.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//q := fmt.Sprintf("SELECT * FROM %s", "container_data")
-	q := fmt.Sprintf("select * from container_data where container = 'practice_container'")
-	res, err := queryDB(c, q)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res[0].Series[0])
-	fmt.Println(res[0].Series[0].Values[0])
-
-	//AddPoint("192.168.12.1", "mycontainer", 0, 0.00012064271, tm)
-}
+// func main() {
+// 	i, err := strconv.ParseInt("1490057610", 10, 64)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	tm := time.Unix(i, 0)
+// 	fmt.Println(tm)
+//
+// 	c, err := influx.NewHTTPClient(influx.HTTPConfig{
+// 		Addr:     "http://localhost:10090",
+// 		Username: username,
+// 		Password: password,
+// 	})
+//
+// 	defer c.Close()
+//
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	//q := fmt.Sprintf("SELECT * FROM %s", "container_data")
+// 	q := fmt.Sprintf("select * from container_data where agent = '192.168.0.26' ORDER BY time DESC LIMIT 6")
+// 	res, err := queryDB(c, q)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	//fmt.Println(res[0].Series[0].Values)
+// 	//fmt.Println(res[0].Series[0].Values[0])
+//
+// 	for _, value := range res[0].Series[0].Values {
+// 		fmt.Printf("%s : %s : %s \n", value[0], value[1], value[2])
+// 	}
+//
+// 	//AddPoint("192.168.12.1", "mycontainer", 0, 0.00012064271, tm)
+// }
